bc85a8bf08740511c2903c618489c51717e2bec9
Adam Havlik <xhavli59@stud.fit.vutbr.cz>
2025-11-16 20:59:16 +0100

    add: New dns_structures for dns_packet, dns_query, BUFFER_SIZE and DNS_HEADER_LENGTH

---
+++ dns_structures.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+#include <netinet/in.h>
+#include <string>
+
+constexpr int BUFFER_SIZE = 512; // Standard DNS packet size over UDP
+constexpr int DNS_HEADER_LENGTH = 12;
+
+
+struct dns_packet {
+    uint8_t data[BUFFER_SIZE];
+    int length{};
+    sockaddr_in clientAddr{};
+    socklen_t clientLen{};
+};
+
+struct dns_query {
+    bool valid = false;
+    bool blocked = false;
+    std::string qname;
+    uint16_t qtype = 0;
+    uint16_t qclass = 0;
+    uint16_t id = 0;
+};
