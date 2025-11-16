842e1eb6400633cf56114f0677420f76b695446a
Adam Havlik <xhavli59@stud.fit.vutbr.cz>
2025-11-16 21:01:02 +0100

    refactor: print_config moved to print_helper

--- proxy_config.h
+++ proxy_config.h
@@ -3,7 +3,6 @@
 #include <string>
 #include <netinet/in.h>
 
-// Proxy configuration structure
 struct proxy_config {
     std::string server;      // Hostname or IP address of real DNS server
     in_addr server_ip;       // IPv4 address of real DNS server
@@ -11,6 +10,3 @@ struct proxy_config {
     std::string filter_file; // Path to filter file
     bool verbose = false;    // Verbose output
 };
-
-// Function prototype
-void print_config(const proxy_config &cfg);
